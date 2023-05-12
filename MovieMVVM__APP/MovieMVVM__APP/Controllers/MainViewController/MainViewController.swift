0f5796690211a15f7299f2073ef407765177e088
Ryan Matheus <t3651512@timbrasil.com.br>
2023-05-12 18:29:52 -0300

    Commit #05 adding Observable to the app and the movie titles to the app too.

--- MovieMVVM__APP/MovieMVVM__APP/Controllers/MainViewController/MainViewController.swift
+++ MovieMVVM__APP/MovieMVVM__APP/Controllers/MainViewController/MainViewController.swift
@@ -11,14 +11,24 @@ class MainViewController: UIViewController {
 
     //IBoutlets:
     
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var tableView: UITableView!
     //ViewModel:
     var viewModel: MainViewModel = MainViewModel()
+    
+    //variables:
+    var cellDataSource: [Movie] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configView()
+        bindViewModel()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        viewModel.getData()
     }
  
     func configView() {
@@ -28,8 +38,26 @@ class MainViewController: UIViewController {
         setupTableView()
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        viewModel.getData()
+    func bindViewModel() {
+        viewModel.isLoading.bind { [weak self] isLoading in
+            guard let self = self, let isLoading = isLoading else {
+                return
+            }
+            DispatchQueue.main.async {
+                if isLoading {
+                    self.activityIndicator.startAnimating()
+                } else {
+                    self.activityIndicator.stopAnimating()
+                }
+            }
+        }
+        
+        viewModel.cellDataSource.bind { [weak self] movies in
+            guard let self = self, let movies = movies else {
+                return
+            }
+            self.cellDataSource = movies
+            self.reloadTableView()
+        }
     }
-    
 }
